a4f582fdd122d467fadf3122a3dc2dc056271b90
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-26 19:34:20 +0200

    Add config files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '936907102685-hqg5j9a2ogm3j5vvbj3ddlcocqpd8589.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': '4EntH3EdKcH8Z3ZhnRAy81Q7',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
